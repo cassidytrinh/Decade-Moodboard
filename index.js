d1caa661b4eef6be86778a06dc455b949a3290d2
Cassidy Trinh <Cassidy@MacBook-Pro.fios-router.home>
2020-05-25 04:38:16 -0400

    new commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function showBoard(event){
   nameO.addClass('.name-output');
   var name=$('.name-input').val();
   var decade=$('.decade-input option:selected').val();
-  nameO.text(`${name}`);
+  nameO.text(`${name}`+" in the " + `${decade}`);
   nameO.addClass('name-'+`${decade}`);
   board.removeClass('board-'+`${old}`).addClass('board-'+`${decade}`);
   board.hide().fadeIn(2000);
